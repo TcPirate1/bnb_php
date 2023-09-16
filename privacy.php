2273f8870cc920aae5a7b023ba3700c5fb4a0220
TC_win_laptop <88120195+TcPirate1@users.noreply.github.com>
2023-09-16 15:38:54 +1200

    create privacy statement and include in the footer. Also added the selenium test suite.

---
+++ privacy.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Privacy Statement</title>
+  </head>
+
+  <?php
+    include "checksession.php";
+    include "header.php";
+    include "menu.php";
+    echo '<div id="site_content">';
+    include "sidebar.php";
+    echo '<div id="content">';
+    ?>
+    <body>
+    <h1>Privacy Statement</h1>
+    <p>
+      We collect personal information from you, including information about your:
+    <br>
+•	Name<br>
+•	Contact information<br>
+•	Email<br>
+We collect your personal information in order to:<br>
+•	identify and book customers into our rooms.<br>
+Providing some information is optional. If you choose not to enter their name and contact details, we'll notify them of changes or provide them with rooms to stay.. We keep your information for five years at which point we securely destroy it by securely erasing all digital information.<br>
+You have the right to ask for a copy of any personal information we hold about you, and to ask for it to be corrected if you think it is wrong. If you’d like to ask for a copy of your information, or to have it corrected, please contact us at ongaongabandb@email.com, or 03-ongaongabandb, or Ongaonga, Hawke's Bay.
+
+    </p>
+    <?php
+    echo '</div></div>';
+    require_once "footer.php";
+    ?>
+  </body>
+  </html>
